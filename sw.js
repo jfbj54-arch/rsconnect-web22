305d3aa7050bc80be79add3dc522c674b4c5b669
jfbj54-arch <jfbj54@gmail.com>
2026-04-24 10:18:03 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,15 @@
-const CACHE_NAME = "rsconnect-cache-v1";
-const FILES_TO_CACHE = [
-  "index.html",
-  "manifest.json",
-  "icon-192.png",
-  "icon-512.png"
-];
-
-// Instalação do Service Worker
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
+self.addEventListener("install", event => {
+    self.skipWaiting();
 });
 
-// Ativação e limpeza de caches antigos
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
+self.addEventListener("activate", event => {
+    clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
         })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-// Intercepta requisições e serve do cache quando possível
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    );
 });
